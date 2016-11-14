5d84632fb86b63a4a6e2ad5503a7fbd892489335
kris <kritika_soni@cmu.ac.th>
2016-11-14 22:48:40 +0700

    Initial commit

---
+++ src/test/java/com/kittysn94/AppTest.java
@@ -0,0 +1,40 @@
+package com.kittysn94;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        WebDriver webDriver = new ChromeDriver();
+    }
+}
