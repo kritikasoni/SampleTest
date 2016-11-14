e4b042dad903f64d04465d57127e7645d6c5c885
kris <kritika_soni@cmu.ac.th>
2016-11-14 23:32:18 +0700

    test open browser and title

--- src/test/java/com/kittysn94/AppTest.java
+++ src/test/java/com/kittysn94/AppTest.java
@@ -5,6 +5,9 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for simple App.
@@ -35,6 +38,14 @@ public class AppTest
      */
     public void testApp()
     {
-        WebDriver webDriver = new ChromeDriver();
+        //chrome
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\dell\\IdeaProjects\\SampleTest\\chromedriver.exe");//set path of driver//ใช้ chrome driver เพื่อสั่งการ chrome browser
+        WebDriver driver = new ChromeDriver(); //สร้าง driver จาก path ด้านบน
+        driver.manage().window().maximize();//ขยายหน้าจอ
+        driver.manage().timeouts().implicitlyWait (30, TimeUnit.SECONDS);//set timeout to wait for 30 sec, if nothing happens, then show error
+        String baseUrl = "http://www.seleniumhq.org";//the url which we will navigate  to
+        driver.get(baseUrl);//สั่งให้ไปหน้านี้นะ
+        assertEquals("Selenium - Web Browser Automation", driver.getTitle()); //assert quals is to test that the expected=actual value
+        driver.quit();//ปิด browser
     }
 }
